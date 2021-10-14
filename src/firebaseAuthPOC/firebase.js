5b5d9e753a1f256a523a5dfd6b77605071f32b5c
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-10-14 22:29:45 +0530

    3

--- src/firebaseAuthPOC/firebase.js
+++ src/firebaseAuthPOC/firebase.js
@@ -1,13 +1,17 @@
 import firebaseConfig from "../secret";
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import 'firebase/compat/storage';
-import 'firebase/compat/firestore'
-const firestore = firebase.firestore();
+import "firebase/auth";
+import 'firebase/storage';
+import 'firebase/firestore'
 firebase.initializeApp(firebaseConfig);
+// firestore -> database user details add
+const firestore = firebase.firestore();
+// asset store 
 export const storage = firebase.storage();
-// collection/>>
+
+// collection>>
 export const database = {
-    users: firestore.collection('users')
+    users: firestore.collection('users'),
+    createdAt: firestore.FieldValue
 }
 export default firebase;
